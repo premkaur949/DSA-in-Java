726dd65aa97bd884e33dfa39f74bc80f7fba0c80
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-03-07 23:21:55 +0530

    AvgTemp.java

--- AvgTemp.java
+++ AvgTemp.java
@@ -10,10 +10,14 @@ public static void main(String[] args) {
     Scanner input=new Scanner(System.in);
         System.out.println("Number of days: ");
         int days=input.nextInt();
-        for(int i=1; i<=days; i++){
-            System.out.println("Temperature on Day "+i+":");
-            Scanner high= new Scanner(System.in);
-            i=high.nextInt();
+        int[] Temp=new int[days];
+        int sum=0;
+            for (int i = 0; i < days; i++) {
+            Scanner consol = new Scanner(System.in);
+            System.out.println("The Temperature of day "+(i+1)+ " is: ");
+            Temp[i]= consol.nextInt();
+            sum=sum+Temp[i];
+
         }
 }
 
