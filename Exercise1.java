57e7bb459a1a6aa82dfd7c930743b6f909b464ed
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-03-09 23:35:54 +0530

    Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,12 @@
+public class Exercise {
+    public static int[] middle(int[] array) {
+        
+        int[] middleArray= new int[array.length-2];
+        
+        for(i=1; i<array.length- 1; i++){
+            System.out.println(middleArray[i]);
+        }
+        
+    }
+
+}
