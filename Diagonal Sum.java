7cc84d9131ac52a5f6d33cc3a1806d7dc8d3d7c3
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-03-10 23:30:32 +0530

    Diagonal Sum.java
    Calculating the sum of diagonal elements in a 2D array.

---
+++ Diagonal Sum.java
@@ -0,0 +1,11 @@
+public class Ex2 {
+ 
+        public static int sumDiagonalElements(int[][] array) {
+          
+          int sum=0;
+          for(int row=0; row<array.length; row++){
+                      sum=sum+array[row][row];
+                  }
+          return sum;
+      }
+}
