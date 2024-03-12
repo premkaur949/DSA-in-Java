e77c9e523e4d049e26eca5249b0d432654b90023
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-03-12 22:58:54 +0530

    Best Score.java
    Printing the First highest and the second highest element form the given array.

--- Best Score.java
+++ Best Score.java
@@ -1,6 +1,20 @@
-public class Ex2 {
-  public static int[] findTopTwoScores(int[] array){
-    
-  }
 
+import java.util.Arrays;
+import java.util.Collections;
+public class Exercise {
+    public static int[] findTopTwoScores(int[] array) {
+        int first=Integer.MIN_VALUE;
+        int second= Integer.MIN_VALUE;
+        
+        for(int score:array){
+            if(score>first){
+                second=first;
+                first=score;
+            }else if(score>second && score<first){
+                second=score;
+            }
+            
+        }
+        return new int[]{first, second};
+    }
 }
