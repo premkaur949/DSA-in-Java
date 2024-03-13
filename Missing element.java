2ec628454e6bb9f9b7f1446053c4cae04796ced7
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-03-13 23:24:33 +0530

    Missing element.java
    Printing the missing element from an array containing consecutive natural numbers.

---
+++ Missing element.java
@@ -0,0 +1,21 @@
+public class Ex3 {
+
+    static int findMissingNumberInArray(int[] arr) {
+        int sum=0;
+        int a=arr.length*(arr.length+1)/2;
+        for (int i = 0; i < arr.length; i++) {
+            sum=sum+i;
+            }
+            int b=a-sum;
+            if (b==0) {
+                System.out.println();
+
+            } else {
+                System.out.println();
+
+            }
+            return b;
+            }
+                
+        }
+   
