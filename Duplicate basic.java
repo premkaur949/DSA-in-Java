a7fc6d8cd12e99e9d64195c60a57af0d1479101c
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-03-20 23:56:52 +0530

    Duplicate basic.java

---
+++ Duplicate basic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class duplicate {
+    public static int[] removeDuplicates(int[]arr){
+       for (int i = 0; i < arr.length; i++) {
+        if(arr[i]==arr[i+1]) {
+        }
+        else{
+            int[] ar=arr;
+        }
+
+       } 
+    }
+}
