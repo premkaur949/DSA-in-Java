0d609ae5bdbfaa3f77e18cae5c9568d9beeea065
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-03-27 00:27:13 +0530

    Update finding number.java

--- finding number.java
+++ finding number.java
@@ -1,19 +1,11 @@
-class Main {
-  public static void main(String[] args) {
-    Main mn = new Main();
-    int[] intArray = {1,2,3,4,5,6};
-    mn.linearSearch(intArray, 7);
+public class Search {
+    public int searchInArray(int[] intArray, int valueToSearch) {
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i]==valueToSearch) {
+                return i;
 
-  }
-
-  // Search Method
-  public void linearSearch(int[] intArray, int value) {
-    for(int i=0; i<intArray.length; i++) {
-      if (intArray[i] == value) {
-        System.out.println("Value is found at the index of " + i);
-        return;
-      }
+            }
+        }throw new IllegalArgumentException("No such number found");
     }
-    System.out.println(value + " is not found");
-  }
+
 }
