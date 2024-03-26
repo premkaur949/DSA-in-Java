85f62535919fc2d987c736d8b977783e7e721083
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-03-26 23:59:10 +0530

    Update Two Sums.java

--- Two Sums.java
+++ Two Sums.java
@@ -1,1 +1,17 @@
-return new int[] {a,b};
+public class Exercise {
+    public int[] twoSum(int[] nums, int target) {
+        
+        
+        for(int a=0; a<nums.length-1; a++){
+            for(int b=1; b<nums.length; b++){
+                if(target==nums[a]+nums[b]){
+                    return new int[] {a,b};
+                }
+
+            }
+            
+        }
+        return new int[] {a,b};
+}
+}
+
