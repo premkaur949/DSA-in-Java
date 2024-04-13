72d86b6c829eabf3b9c4a82a668ff339b892c9ea
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-04-13 23:56:44 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -2,7 +2,9 @@
 import java.util.*;
 public class main {
     public static void main(String[] args) {
-        ArrayList ar = new ArrayList(Arrays.asList(13,12,12));
-        System.out.println(ar);
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(1,4);
+        System.out.println(numbers);
     }
 }
