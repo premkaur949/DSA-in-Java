8a618423be693bdc51285c0fd83a4b7ae8e6ded7
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-04-16 23:51:18 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -5,6 +5,7 @@ public class main {
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         numbers.add(1);
         numbers.add(1,4);
+        numbers.add(1,4,6);
         System.out.println(numbers);
     }
 }
