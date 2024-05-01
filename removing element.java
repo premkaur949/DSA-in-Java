a96372dcc71699a5726ba301c82ec0b64292dee2
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-05-01 23:57:56 +0530

    removing element.java

---
+++ removing element.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class RemoveElementsFromArrayListExample {
+    public static void main(String[] args) {
+        List<String> programmingLanguages = new ArrayList<>();
+        programmingLanguages.add("C");
+        programmingLanguages.add("C++");
+        programmingLanguages.add("Java");
+        programmingLanguages.add("Kotlin");
+        programmingLanguages.add("Python");
+        programmingLanguages.add("Perl");
+        programmingLanguages.add("Ruby");
+
+        System.out.println("Initial List: " + programmingLanguages);
+      
+        programmingLanguages.remove(5);
+        System.out.println("After remove(5): " + programmingLanguages);
+
+    }
+}
